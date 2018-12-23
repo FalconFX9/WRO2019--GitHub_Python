d0e34a9c373ac0961d9f80d57105808ee9a0cf81
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2018-12-23 17:56:42 -0500

    Testing the hitechnic sensor

--- run_motor.py
+++ run_motor.py
@@ -3,7 +3,7 @@ import time
 
 mA = Motor(OUTPUT_A)
 cl = Sensor(address='in2:i2c1', driver_name='ht-nxt-color-v2')
-cl.mode = 'NORM', 'value3'
+cl.mode = 'WHITE'
 
 if cl.value < 40:
     mA.run_forever()
