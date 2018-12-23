554468d06b208505e6ec7303d6383b614a946a4a
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2018-12-23 17:54:09 -0500

    Testing the hitechnic sensor

--- run_motor.py
+++ run_motor.py
@@ -2,12 +2,10 @@ from ev3dev.auto import *
 import time
 
 mA = Motor(OUTPUT_A)
-cl = Sensor(address='in2:i2c')
+cl = Sensor(address='in2:i2c1', driver_name='ht-nxt-color-v2')
 cl.mode = 'white'
 
 if cl.value < 40:
     mA.run_forever()
 else:
     mA.stop()
-
-mA.stop(stop_action='brake')
