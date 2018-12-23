1eb257ac3bdd3ce698bef7ba6c9ebbe694236b90
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2018-12-23 18:22:28 -0500

    Testing the hitechnic color sensor

--- run_motor.py
+++ run_motor.py
@@ -3,7 +3,7 @@ from time import sleep
 
 mA = Motor(OUTPUT_A)
 cl = Sensor('in2')
-cl.mode = 'WHITE', 'value0'
+
 while True:
-    print(cl.value())
+    print(cl.reflected_light_intensity)
     sleep(1)
