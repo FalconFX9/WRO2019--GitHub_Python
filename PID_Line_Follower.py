650f1f70cec580f43803e8fb2b24f3bd3f5dc5a4
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2018-12-23 22:26:30 -0500

    PID Line Follower done, PID not adjusted

--- PID_Line_Follower.py
+++ PID_Line_Follower.py
@@ -1,8 +1,22 @@
 from ev3dev2.auto import *
 from time import sleep
 
+# Defining the variables necessary to PID
+# Target is the target value for the sensor (the one it gets when half of it is on the line and half of it is off)
+
 Target = 50
-Kp = 0
+Error = 0
+Last_Error = 0
+Integral = 0
+Derivative = 0
+
+# PID Values --These are subjective and need to be tuned to the robot and mat
+# Kp must be augmented or decreased until the robot follows the line smoothly --Higher Kp = Stronger corrections
+# Same with Ki, after Kp is done
+# Kd to 1, and move up or done until smooth, after Kp and Ki
+# This process can take a VERY long time to fine-tune
+
+Kp = 1
 Ki = 0
 Kd = 0
 
@@ -20,15 +34,14 @@ steer_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
 # PID Line Follower (1 sensor)
 
 
-def pidlinefollower(sensor=Hitechnic1):
-
-    print(sensor)
-    print(sensor.value(3))
-#    target2 = Target - sensor.value(3)
-#   steer_pair.run_forever(steering=target2*Kp, speed=100)
+def pidlinefollower(sensor=Hitechnic1, side=1):
+    global Target, Error, Last_Error, Integral, Derivative, Kp, Ki, Kd
+    Error = Target - sensor.value(3)
+    Integral = Error + Integral
+    Derivative = Error - Last_Error
+    steer_pair.run_forever(steering=((Error * Kp) + (Integral * Ki) + Derivative * Kd)*side, speed=100)
+    Last_Error = Error
     return
 
 
-pidlinefollower(sensor=Hitechnic1)
-sleep(5)
-pidlinefollower(sensor=Hitechnic2)
+
