c7f44f45ff43cb34b183b9d0cfd1c09ae1188d23
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-01-30 17:14:27 -0500

    Debugging
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- sensor_testing.py
+++ sensor_testing.py
@@ -8,9 +8,9 @@ time_duration = time.time() + 5
 # Defining Sensors
 
 Sensor1 = Sensor('in1')
-Sensor2 = Sensor('in2')
-Sensor3 = Sensor('in3')
-Sensor4 = ColorSensor('in4')
+Sensor2 = Sensor('in1')
+Sensor3 = Sensor('in1')
+#Sensor4 = ColorSensor('in4')
 
 # Setting Sensor mode
 
@@ -34,8 +34,9 @@ print('Third Sensor Value')
 for x in range(1, 50):
     print(Sensor3.value(3))
     sleep(0.1)
-
+'''
 print('Fourth Sensor Value')
 for x in range(1, 50):
     print(Sensor4.reflected_light_intensity)
     sleep(0.1)
+'''
