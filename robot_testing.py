135bbcba6b33584ded7f3936ff8820fb118573da
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-02-01 11:56:48 -0500

    Adjusting values for sensors and other stuff
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- robot_testing.py
+++ robot_testing.py
@@ -15,11 +15,10 @@ try:
 except DeviceNotFound:
     print('Main servo not found')
 
-steer_pair.on_for_seconds(steering=0, speed=-40, seconds=3)
 grabber_servo.on_for_rotations(-100, 10)
 if grabber_servo.is_stalled:
     grabber_servo.off(brake=True)
     sleep(5)
-grabber_servo.on_for_rotations(100, 11)
+grabber_servo.on_for_rotations(100, 7)
 if grabber_servo.is_stalled:
     grabber_servo.off(brake=True)
