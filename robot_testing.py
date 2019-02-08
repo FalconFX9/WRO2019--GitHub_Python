367233a64a4996343e3e4030cc82718c2b27b911
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-02-08 16:36:37 -0500

    General optimization
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- robot_testing.py
+++ robot_testing.py
@@ -23,7 +23,7 @@ if grabber_servo.is_stalled:
 
 steer_pair.on_for_rotations(0, -20, 1)
 
-grabber_servo.on_for_rotations(100, 10)
+grabber_servo.on_for_rotations(100, 0)
 if grabber_servo.is_stalled:
     grabber_servo.off(brake=True)
     sleep(5)
