25e2ba6d2c2ea7c1f94b341832cb8c0d358f9264
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-02-14 15:36:48 -0500

    Reorganized and renammed a few files
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- start_sequence.py
+++ start_sequence.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from sensor_and_motor_startup import *
 from line_follower_class import *
+from time import *
 
 sensor_declaration()
 motor_initialization()
@@ -8,7 +9,7 @@ motor_initialization()
 
 def start_sequence():
     follower = OneSensorLineFollower(side_color_sensor, steer_pair)
-    while hitechnic_2.value(3) > 30:
+    while time.time() > time.time() + 10:
         follower.follower()
         if not side_color_sensor.value() == 0:
             firstblock = side_color_sensor.value()
