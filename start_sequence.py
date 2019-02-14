c0e57592a044fb7acb561f873eacfce1ea25affb
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-02-14 15:51:22 -0500

    Reorganized and renammed a few files
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- start_sequence.py
+++ start_sequence.py
@@ -12,15 +12,23 @@ def start_sequence():
     while time() < time() + 10:
         pid_line_follower(speed=30)
         if not side_color_sensor.value() == 0:
+            steer_pair.off()
             firstblock = side_color_sensor.value()
+            steer_pair.on_for_rotations(0, -20, 0.2)
             if not side_color_sensor.value() == 0 or side_color_sensor.value() == firstblock:
+                steer_pair.off()
                 secondblock = side_color_sensor.value()
+                steer_pair.on_for_rotations(0, -20, 0.2)
                 if not side_color_sensor.value() == 0 or side_color_sensor.value() == firstblock or \
                         side_color_sensor.value() == secondblock:
+                    steer_pair.off()
                     thirdblock = side_color_sensor.value()
+                    steer_pair.on_for_rotations(0, -20, 0.2)
                     if not side_color_sensor.value() == 0 or side_color_sensor.value() == firstblock or \
                             side_color_sensor.value() == secondblock or side_color_sensor.value() == thirdblock:
+                        steer_pair.off()
                         fourthblock = side_color_sensor.value()
+                        steer_pair.on_for_rotations(0, -20, 0.2)
                         print(firstblock)
                         print(secondblock)
                         print(thirdblock)
