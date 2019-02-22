8349d005b9adb6c880eaaffae41bc7235d5389e3
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-02-22 17:07:09 -0500

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- robot_testing.py
+++ robot_testing.py
@@ -1,29 +1,8 @@
 #!/usr/bin/env python3
-from ev3dev2.auto import *
-import time
-from time import sleep
+from sensor_and_motor_startup import *
 
-timelimit = time.time() + 5
-steer_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
-try:
-    steer_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
-except DeviceNotFound:
-    print('Main motors not found')
+sensor_declaration()
+motor_initialization()
 
-grabber_servo = MediumMotor(OUTPUT_A)
-try:
-    grabber_servo = MediumMotor(OUTPUT_A)
-except DeviceNotFound:
-    print('Main servo not found')
-
-grabber_servo.on_for_rotations(-100, 10)
-if grabber_servo.is_stalled:
-    grabber_servo.off(brake=True)
-    sleep(1)
-
-steer_pair.on_for_rotations(0, -20, 1)
-
-grabber_servo.on_for_rotations(100, 0)
-if grabber_servo.is_stalled:
-    grabber_servo.off(brake=True)
-    sleep(5)
+while time.time() < time.time() + 10:
+    stock_pid_follower(sensor=line_1, side=1, speed=80)
