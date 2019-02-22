9406564929adda1d45ec585323399edfe77d466c
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-02-22 16:44:09 -0500

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- start_sequence.py
+++ start_sequence.py
@@ -11,35 +11,34 @@ fourthblock = 0
 def start_sequence():
     global fourthblock
     lower_motor.off(brake=True)
-    while fourthblock == 0:
-        stock_pid_follower(sensor=line_1, speed=30, side=1)
+    stock_pid_follower(sensor=line_1, speed=30, side=1)
+    if not side_color_sensor.value() == 0:
+        steer_pair.off()
+        firstblock = side_color_sensor.value()
+        print(firstblock)
+        sleep(4)
+        steer_pair.on_for_rotations(0, -20, 0.2)
         if not side_color_sensor.value() == 0:
             steer_pair.off()
-            firstblock = side_color_sensor.value()
-            print(firstblock)
+            secondblock = side_color_sensor.value()
+            print(secondblock)
             sleep(4)
             steer_pair.on_for_rotations(0, -20, 0.2)
             if not side_color_sensor.value() == 0:
                 steer_pair.off()
-                secondblock = side_color_sensor.value()
-                print(secondblock)
+                thirdblock = side_color_sensor.value()
+                print(thirdblock)
                 sleep(4)
                 steer_pair.on_for_rotations(0, -20, 0.2)
                 if not side_color_sensor.value() == 0:
                     steer_pair.off()
-                    thirdblock = side_color_sensor.value()
-                    print(thirdblock)
-                    sleep(4)
+                    fourthblock = side_color_sensor.value()
+                    print(fourthblock)
                     steer_pair.on_for_rotations(0, -20, 0.2)
-                    if not side_color_sensor.value() == 0:
-                        steer_pair.off()
-                        fourthblock = side_color_sensor.value()
-                        print(fourthblock)
-                        steer_pair.on_for_rotations(0, -20, 0.2)
-                        print("Block 1: ", firstblock)
-                        print("Block 2 : ", secondblock)
-                        print("Block 3 : ", thirdblock)
-                        print("Block 4 : ", fourthblock)
+                    print("Block 1: ", firstblock)
+                    print("Block 2 : ", secondblock)
+                    print("Block 3 : ", thirdblock)
+                    print("Block 4 : ", fourthblock)
 
     steer_to_line(100, -60, line_1)
     steer_pair.off()
