246c5686018bb6ab133095dc81f52548421ba26a
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-02-22 17:08:44 -0500

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- robot_testing.py
+++ robot_testing.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from time import *
 from sensor_and_motor_startup import *
 
 sensor_declaration()
 motor_initialization()
 
-while time.time() < time.time() + 10:
+while time() < time() + 10:
     stock_pid_follower(sensor=line_1, side=1, speed=80)
