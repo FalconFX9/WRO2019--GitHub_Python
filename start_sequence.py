b6484ccfa5d757c0b75e4f7c6511a61bfd7d2c8e
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-02-22 16:18:42 -0500

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- start_sequence.py
+++ start_sequence.py
@@ -12,20 +12,23 @@ def start_sequence():
     global fourthblock
     lower_motor.off(brake=True)
     while fourthblock == 0:
+        print(side_color_sensor.value())
         stock_pid_follower(sensor=line_1, speed=30, side=1)
         if not side_color_sensor.value() == 0:
             steer_pair.off()
             firstblock = side_color_sensor.value()
             steer_pair.on_for_rotations(0, -20, 0.2)
-        if not side_color_sensor.value() == 0:
+        if not side_color_sensor.value() == 0 and not side_color_sensor.value() == firstblock:
                 steer_pair.off()
-                secondblock = side_color_sensor.value  ()
+                secondblock = side_color_sensor.value()
                 steer_pair.on_for_rotations(0, -20, 0.2)
-        if not side_color_sensor.value() == 0:
+        if not side_color_sensor.value() == 0 and not side_color_sensor.value() == secondblock \
+                and not side_color_sensor.value() == firstblock:
                     steer_pair.off()
                     thirdblock = side_color_sensor.value()
                     steer_pair.on_for_rotations(0, -20, 0.2)
-        if not side_color_sensor.value() == 0:
+        if not side_color_sensor.value() == 0 and not side_color_sensor.value() == firstblock \
+                and not side_color_sensor.value() == secondblock and not side_color_sensor.value() == thirdblock:
                         steer_pair.off()
                         fourthblock = side_color_sensor.value()
                         steer_pair.on_for_rotations(0, -20, 0.2)
