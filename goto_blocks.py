f97f1e1793dbd9e39bbace9fbea625794219ee4f
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-06 16:10:06 -0500

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- goto_blocks.py
+++ goto_blocks.py
@@ -2,9 +2,9 @@ from sensor_and_motor_startup import *
 
 
 def goto_blocks():
-    timemax = time() + 5
+    timemax = time() + 3
     while time() < timemax:
-        stock_pid_follower(sensor=line_1, speed=40, side=-1)
+        stock_pid_follower(sensor=line_1, speed=60, side=-1)
     steer_pair.off()
 
 
