1febf867c431f116a670a14c7e331f0d7c19d9f7
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-06 15:47:49 -0500

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- goto_blocks.py
+++ goto_blocks.py
@@ -2,6 +2,6 @@ from sensor_and_motor_startup import *
 
 
 def goto_blocks():
-    while not time.time() < time.time() + 5:
-        stock_pid_follower(sensor=line_1, speed=60)
+    while not time() < time() + 5:
+        stock_pid_follower(sensor=line_1, speed=60, side=0)
     steer_pair.off()
