c5216c1cce0bd68cb08a501b36f194cbd15d2c2d
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-12 15:41:36 -0400

    Modifying threading
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- goto_blocks.py
+++ goto_blocks.py
@@ -1,10 +1,10 @@
 from sensor_and_motor_startup import *
+from line_follower_class import *
 
 
 def goto_blocks():
-    timemax = time() + 3
-    while time() < timemax:
-        stock_pid_follower(sensor=center_sensor, speed=60, side=-1)
+    high_speed_follower()
+    #stock_pid_follower(sensor=center_sensor, speed=60, side=-1)
     steer_pair.off()
 
 
