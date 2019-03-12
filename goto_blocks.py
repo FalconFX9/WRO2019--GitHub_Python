11fab41e45a0cf32c8c5de0101db07a29d3b8426
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-12 15:59:16 -0400

    Testing line following
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- goto_blocks.py
+++ goto_blocks.py
@@ -3,7 +3,6 @@ from line_follower_class import *
 
 
 def goto_blocks():
-    old_pos = steer_pair.position()
     while not steer_pair.wait_until_not_moving(motors=steer_pair):
         high_speed_follower()
     #stock_pid_follower(sensor=center_sensor, speed=60, side=-1)
