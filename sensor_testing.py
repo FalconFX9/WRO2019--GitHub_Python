8d0c5094bf2dc47d1daa33c77a0e4dc953314844
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-13 17:56:16 -0400

    Programming the robot
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- sensor_testing.py
+++ sensor_testing.py
@@ -11,14 +11,6 @@ motor_initialization()
 
 # SSH and on Brick Debugging
 
-grabber_servo.on_for_rotations(-100, 10)
-if grabber_servo.is_stalled:
-    grabber_servo.off(brake=True)
-    sleep(5)
-grabber_servo.on_for_rotations(100, 11)
-if grabber_servo.is_stalled:
-    grabber_servo.off(brake=True)
-
 print('First Sensor Value')
 for x in range(1, 50):
     print(left_side_sensor.value(3))
@@ -26,7 +18,7 @@ for x in range(1, 50):
 
 print('Second Sensor Value')
 for x in range(1, 50):
-    print(hitechnic_2.value(3))
+    print(right_side_sensor.value(3))
     sleep(0.1)
 
 print('Third Sensor Value')
@@ -36,5 +28,5 @@ for x in range(1, 50):
 
 print('Fourth Sensor Value')
 for x in range(1, 50):
-    print(color_rear.reflected_light_intensity)
+    print(center_sensor.reflected_light_intensity)
     sleep(0.1)
