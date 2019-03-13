cae827595286dc69e7bb9c7ab6d5846b055b6e9a
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-13 17:38:05 -0400

    Programming the robot
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- start_sequence.py
+++ start_sequence.py
@@ -45,7 +45,10 @@ def start_sequence():
 
     follow_to_line(following_sensor=right_side_sensor, side_of_line=1, speed=40)
     steer_pair.off()
-    steer_pair.on_for_rotations(100, -30, 0.8)
+    steer_pair.on_for_rotations(70, -30, 1.4)
 
 
 start_sequence()
+steer_pair.off(brake=False)
+lower_motor.off(brake=False)
+grabber_servo.off(brake=False)
