7adff6e6a2b85148103e0aa7e2ced6e8237f299f
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-13 17:58:27 -0400

    Programming the robot
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- sensor_testing.py
+++ sensor_testing.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
 from sensor_and_motor_startup import *
 
-time_duration = time.time() + 5
-
-
 # Defining Sensors
 
 sensor_declaration()
 motor_initialization()
 
 # SSH and on Brick Debugging
-
+"""
 print('First Sensor Value')
 for x in range(1, 50):
     print(left_side_sensor.value(3))
     sleep(0.1)
-
+"""
 print('Second Sensor Value')
 for x in range(1, 50):
     print(right_side_sensor.value(3))
     sleep(0.1)
 
+"""
 print('Third Sensor Value')
 for x in range(1, 50):
     print(side_color_sensor.value(3))
@@ -30,3 +28,4 @@ print('Fourth Sensor Value')
 for x in range(1, 50):
     print(center_sensor.reflected_light_intensity)
     sleep(0.1)
+"""
