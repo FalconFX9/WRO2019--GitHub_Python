01d81023dc90307157d4b33136d1d35d767e4626
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-14 18:01:02 -0400

    Modifying the code to put down the cable
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- sensor_testing.py
+++ sensor_testing.py
@@ -7,15 +7,15 @@ sensor_declaration()
 motor_initialization()
 
 # SSH and on Brick Debugging
-"""
+
 print('First Sensor Value')
 for x in range(1, 50):
-    print(left_side_sensor.value(3))
+    print(left_side_sensor.value(0))
     sleep(0.1)
-"""
+
 print('Second Sensor Value')
 for x in range(1, 50):
-    print(right_side_sensor.value(3))
+    print(right_side_sensor.value(0))
     sleep(0.1)
 
 """
