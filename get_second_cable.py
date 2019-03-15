39a9c0cc997c89022af47fe0add9f289dc22e9bd
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-15 15:42:27 -0400

    Changing the code to get to the second cable
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- get_second_cable.py
+++ get_second_cable.py
@@ -31,7 +31,7 @@ def get_second_cable():
             losp_right_follower()
         lines_passed = False
         steer_pair.off()
-        steer_pair.on_for_rotations(-100, -20, 0.75)
+        steer_pair.on_for_rotations(-100, -20, 0.78)
         t = Thread(target=check_for_lines, args=(1, ))
         t.start()
         while not lines_passed:
