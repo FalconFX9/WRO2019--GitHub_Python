907a293d498a404508e4a53bcf6e82b9669174a0
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-15 15:22:17 -0400

    Created the code to go get the second cable.
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

---
+++ get_second_cable.py
@@ -0,0 +1,45 @@
+from line_follower_class import *
+from threading import *
+
+lines_passed = True
+
+
+def get_second_cable():
+
+    def check_for_lines(num_lines):
+        global lines_passed
+        counter = 0
+        while counter < num_lines:
+            if center_sensor.reflected_light_intensity < 30:
+                if counter < num_lines - 1:
+                    beep = Sound()
+                    counter = counter + 1
+                    beep.beep()
+                    sleep(0.3)
+                else:
+                    counter = counter + 1
+            print(counter)
+        lines_passed = True
+
+    def get_cable():
+        steer_pair.on_for_rotations(-100, -20, 0.68)
+        t = Thread(target=check_for_lines, args=(2, ))
+        t.start()
+        while not lines_passed:
+            losp_right_follower()
+        steer_pair.off()
+        steer_pair.on_for_rotations(-100, -20, 0.68)
+        t = Thread(target=check_for_lines, args=(1, ))
+        t.start()
+        while not lines_passed:
+            hisp_left_follower(side_of_line=1, speed=40)
+        steer_pair.off()
+
+    def pick_up_cable():
+        lower_motor.on_for_degrees(speed=10, degrees=90)
+        timed_follower(center_sensor, speed=20, timemax=0.85)
+        steer_pair.off()
+        lower_motor.on_for_degrees(speed=10, degrees=-90)
+
+    get_cable()
+    pick_up_cable()
