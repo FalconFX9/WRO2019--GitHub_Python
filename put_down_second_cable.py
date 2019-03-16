dc2986d0116c32bc18b5a91ac952ac783011801a
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-16 13:37:52 -0400

    Adjusting turns and minor corrections.
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- put_down_second_cable.py
+++ put_down_second_cable.py
@@ -32,7 +32,7 @@ def put_down_second_cable():
         lines_passed = False
         while center_sensor.reflected_light_intensity > 30:
             steer_pair.on(100, -30)
-        steer_pair.on_for_rotations(100, -20, 0.15)
+        steer_pair.on_for_rotations(100, -20, 0.2)
         t = Thread(target=check_for_lines, args=(2,))
         t.start()
         while not lines_passed:
