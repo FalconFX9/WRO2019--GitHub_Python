5c445f969daebeb93acc9cf8ecbe9cd0ec9a1115
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-20 16:57:28 -0400

    Adjusting turns and minor corrections.
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- goto_blocks.py
+++ goto_blocks.py
@@ -38,7 +38,7 @@ def goto_cables_group():
         lower_motor.on_for_degrees(speed=10, degrees=-90)
 
     def turn_around():
-        steer_pair.on_for_rotations(-50, 30, 0.8)
+        steer_pair.on_for_rotations(-50, 100, 2)
 
     goto_cable()
     pick_up_cable()
