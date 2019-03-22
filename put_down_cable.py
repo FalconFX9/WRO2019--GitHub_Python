14c90311a71ba3f7012874d1a21b4f5d38bb64a0
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-22 17:01:19 -0400

    Minor corrections
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- put_down_cable.py
+++ put_down_cable.py
@@ -30,7 +30,7 @@ def put_down_cable():
             hisp_left_follower(side_of_line=1, speed=40)
         steer_pair.off()
         steer_pair.on_for_rotations(0, -40, 0.3)
-        steer_pair.on_for_rotations(-100, -20, 0.68)
+        steer_pair.on_for_rotations(-100, -20, 0.6)
         lines_passed = False
         t = Thread(target=check_for_lines, args=(2,))
         t.start()
@@ -38,7 +38,7 @@ def put_down_cable():
             losp_right_follower()
         steer_pair.off()
         sleep(3)
-        steer_pair.on_for_rotations(0, -30, 0.1)
+        steer_pair.on_for_rotations(0, -30, 0.3)
         sleep(3)
         while center_sensor.reflected_light_intensity > 30:
             steer_pair.on(75, -30)
