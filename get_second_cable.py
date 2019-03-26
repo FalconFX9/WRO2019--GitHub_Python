3141b9f2843614ae34c9001131457d1d72e07e2c
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-26 15:40:51 -0400

    Experimental code for putting down the blocks. Added movement, but is untested
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- get_second_cable.py
+++ get_second_cable.py
@@ -25,8 +25,7 @@ def get_second_cable():
         global lines_passed
         left_side_sensor.mode = 'COL-REFLECT'
         right_side_sensor.mode = 'COL-REFLECT'
-        delai = time() + 0.3
-        while right_side_sensor.reflected_light_intensity > 30 and time() > delai:
+        while right_side_sensor.reflected_light_intensity > 30:
             steer_pair.on(-100, -20)
         steer_pair.off()
         t = Thread(target=check_for_lines, args=(2, ))
@@ -36,8 +35,6 @@ def get_second_cable():
         lines_passed = False
         steer_pair.off()
         steer_pair.on_for_rotations(-100, -20, 0.7)
-        steer_pair.off()
-        steer_pair.on_for_rotations(-100, -20, 0.1)
         t = Thread(target=check_for_lines, args=(1, ))
         t.start()
         while not lines_passed:
