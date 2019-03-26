6e601091aa530a29daf8a0a27214aa9389192007
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-26 15:57:16 -0400

    Iterative improvements
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- get_second_cable.py
+++ get_second_cable.py
@@ -32,10 +32,10 @@ def get_second_cable():
         t = Thread(target=check_for_lines, args=(2, ))
         t.start()
         while not lines_passed:
-            losp_right_follower()
+            losp_right_follower(speed=30)
         lines_passed = False
         steer_pair.off()
-        steer_pair.on_for_rotations(-70, -60, 0.8)
+        steer_pair.on_for_rotations(-70, -60, 0.78)
         t = Thread(target=check_for_lines, args=(1, ))
         t.start()
         while not lines_passed:
