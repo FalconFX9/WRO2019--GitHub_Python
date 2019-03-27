697cda10d996537d24a0f65b6504b9abdc82a6ce
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-27 16:15:24 -0400

    Iterative improvements
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- put_down_cable.py
+++ put_down_cable.py
@@ -49,6 +49,7 @@ def put_down_cable():
         right_side_sensor.mode = 'COL-COLOR'
         while not (left_side_sensor.value() == 5 and right_side_sensor.value() == 6 and time() > wait):
             losp_center_follower(side_of_line=1)
+            print(center_sensor.reflected_light_intensity)
         steer_pair.off()
         lower_motor.on_for_degrees(speed=10, degrees=90)
         steer_pair.on_for_rotations(0, 30, 0.5)
