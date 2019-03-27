947af89f26d6e2b2a127c7ef3eaa250c1500b403
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-27 16:03:04 -0400

    Iterative improvements
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- turn_test.py
+++ turn_test.py
@@ -1,4 +1,5 @@
 from line_follower_class import *
 
 lower_motor.off()
-steer_pair.on_for_rotations(-100, 40, 1.2)
+steer_pair.on_for_rotations(-50, 40, 0.6)
+steer_pair.on_for_rotations(50, -40, 0.8)
