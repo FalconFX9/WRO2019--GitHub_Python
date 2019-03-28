56cdb0c1f2d3793f07d38c266b5ea5f13f18fdc9
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-28 15:13:34 -0400

    Added sensor plotting for debug
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- sensor_plotting.py
+++ sensor_plotting.py
@@ -1,6 +1,6 @@
 import datetime as dt
 import matplotlib.pyplot as plt
-from time import sleep
+from line_follower_class import *
 
 # Create figure for plotting
 fig = plt.figure()
@@ -9,26 +9,28 @@ xs = []
 ys = []
 ts = []
 
-# Sample reflected light intensity every 100th of a second for 10 seconds
-for t in range(0, 1000):
 
-    # Add x and y to lists
-    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-    ys.append(t)
-    ts.append(35)
+def sensor_plotting(sensor=center_sensor):
+    # Sample reflected light intensity every 100th of a second for 10 seconds
+    for t in range(0, 1000):
 
-    # Wait 0.01 second before sampling light intensity again
-    sleep(0.01)
+        # Add x and y to lists
+        xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+        ys.append(sensor)
+        ts.append(35)
 
-# Draw plot
-ax.plot(xs, ys)
-ax.plot(xs, ts)
+        # Wait 0.01 second before sampling light intensity again
+        sleep(0.01)
 
-# Format plot
-plt.xticks(rotation=45, ha='right')
-plt.subplots_adjust(bottom=0.30)
-plt.title('Sensor value compared to target')
-plt.ylabel('Reflected light intensity %')
+    # Draw plot
+    ax.plot(xs, ys)
+    ax.plot(xs, ts)
 
-# Draw the graph
-plt.show()
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('Sensor value compared to target')
+    plt.ylabel('Reflected light intensity %')
+
+    # Draw the graph
+    plt.show()
