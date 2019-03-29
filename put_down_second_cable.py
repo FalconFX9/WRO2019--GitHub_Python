599d9efd9fc6c7fcd413b445d9d6b447b76139f3
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-29 16:20:20 -0400

    Added plotting via downloadable text files
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- put_down_second_cable.py
+++ put_down_second_cable.py
@@ -36,7 +36,7 @@ def put_down_second_cable():
             print(center_sensor.reflected_light_intensity)
         steer_pair.off()
         steer_pair.on_for_rotations(100, -20, 0.2)
-        t = Thread(target=check_for_lines, args=(1,))
+        t = Thread(target=check_for_lines, args=(2,))
         t.start()
         while not lines_passed:
             losp_right_follower()
