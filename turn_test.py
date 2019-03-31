5f25c1a306939bc03d43037f3d5ab1dae017dd12
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-31 17:43:38 -0400

    Logging data
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- turn_test.py
+++ turn_test.py
@@ -1,4 +1,10 @@
 from line_follower_class import *
 
 lower_motor.off(brake=True)
-steer_pair.on_for_rotations(60, 40, 1)
+lower_motor.on_for_degrees(10, -10)
+steer_pair.on_for_rotations(60, 40, 1.06)
+steer_pair.on_for_rotations(0, 20, 0.3)
+lower_motor.on_for_degrees(10, 65)
+grabber_servo.on_for_degrees(10, -90)
+timed_follower(sensor=center_sensor, timemax=0.5, speed=20)
+lower_motor.on_for_degrees(10, -75)
