38632f246e2d66d51e8cf0a5121b052c566ab2e9
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-31 18:38:06 -0400

    Logging data
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- turn_test.py
+++ turn_test.py
@@ -1,11 +1,17 @@
 from line_follower_class import *
 
 lower_motor.off(brake=True)
-lower_motor.on_for_degrees(10, -10)
-steer_pair.on_for_rotations(60, 40, 1.06)
-steer_pair.on_for_rotations(0, 20, 0.2)
-lower_motor.on_for_degrees(10, 56)
-grabber_servo.on_for_degrees(10, -90)
-timed_follower(sensor=center_sensor, timemax=0.65, speed=20, kp=0.2, side_of_line=1)
-steer_pair.off()
-lower_motor.on_for_degrees(10, -75)
+
+while not side_color_sensor.value() == 0:
+    hisp_right_follower(speed=30)
+
+
+def turn_and_pick_up():
+    lower_motor.on_for_degrees(10, -10)
+    steer_pair.on_for_rotations(60, 40, 1.06)
+    steer_pair.on_for_rotations(0, 20, 0.2)
+    lower_motor.on_for_degrees(10, 56)
+    grabber_servo.on_for_degrees(10, -90)
+    timed_follower(sensor=center_sensor, timemax=0.65, speed=20, kp=0.2, side_of_line=1)
+    steer_pair.off()
+    lower_motor.on_for_degrees(10, -75)
