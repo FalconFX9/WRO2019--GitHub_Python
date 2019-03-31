440b89d40a9c0ddab55d0d882af832214e5d7907
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-03-31 18:55:09 -0400

    Logging data
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- turn_test.py
+++ turn_test.py
@@ -2,7 +2,7 @@ from line_follower_class import *
 
 lower_motor.off(brake=True)
 side_color_sensor.mode = 'RGB'
-while 100 > side_color_sensor.value() > 40 and center_sensor.reflected_light_intensity > 30:
+while not (100 > side_color_sensor.value() > 40 and center_sensor.reflected_light_intensity < 30):
     hisp_right_follower(speed=20)
 steer_pair.off()
 
