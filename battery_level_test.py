40955a8e02500215f7887e829938b7c8b530da92
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-12 16:42:13 -0400

    Adding low battery compensation
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- battery_level_test.py
+++ battery_level_test.py
@@ -5,5 +5,5 @@ def check_battery():
 
     battery = open("/sys/devices/platform/battery/power_supply/lego-ev3-battery/voltage_now")
     text = battery.read()
-    return text
+    return int(text)
 
