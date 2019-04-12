7f83bedc8e2849bf9f714a57fb88cc5e93f7a086
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-12 15:27:39 -0400

    Changed robot chassis, readapting code
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- battery_level_test.py
+++ battery_level_test.py
@@ -5,4 +5,8 @@ def check_battery():
 
     battery = open("/sys/devices/platform/battery/power_supply/lego-ev3-battery/voltage_now")
     for i in range(0, 100):
-        print(battery.read())
+        text = battery.read()
+        print(text)
+
+
+check_battery()
