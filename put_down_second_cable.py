8f7cda1b83712e28916d743cde82ff1cd73180e4
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-17 19:30:16 -0400

    Much better line sensing to see where to stop.
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- put_down_second_cable.py
+++ put_down_second_cable.py
@@ -23,8 +23,9 @@ def put_down_second_cable():
     def goto_drop():
         global lines_passed, log_to_files
         steer_pair.on_for_rotations(70, 40, 1.56)
+        sleep(5)
         while right_side_sensor.reflected_light_intensity > 30:
-            steer_pair.on(70, 20)
+            steer_pair.on(70, 10)
         steer_pair.off()
         steer_pair.on_for_rotations(70, 40, 0.07)
         follow_to_line(following_sensor=right_side_sensor, line_sensor=center_sensor, side_of_line=1, speed=40,
