17a9ef50411575a0d71b36e30da2d5caedcc9a4a
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-17 19:35:57 -0400

    Much better line sensing to see where to stop.
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- line_follower_testing.py
+++ line_follower_testing.py
@@ -19,7 +19,7 @@ if input("Do a run with default K_D ? (Y/N) ") == 'Y':
     steer_pair.off()
 print('Change K_D to input value')
 K_D = float(input('Please enter a new K_D value here : '))
-timed_follower(sensor=center_sensor, side_of_line=1, timemax=4, kp=0.3, ttarget=45, kd=K_D)
+follow_for_xlines(4, sensor=right_side_sensor, side_of_line=1, ttarget=45, kd=K_D)
 steer_pair.off()
 
 while True:
@@ -29,5 +29,5 @@ while True:
         kp = float(input('Enter a new K_P value here : '))
     else:
         kp = 0.3
-    timed_follower(sensor=center_sensor, side_of_line=1, timemax=4, kp=kp, ttarget=45, kd=K_D)
+    follow_for_xlines(4, sensor=right_side_sensor, side_of_line=1, ttarget=45, kp=kp, kd=K_D)
     steer_pair.off()
