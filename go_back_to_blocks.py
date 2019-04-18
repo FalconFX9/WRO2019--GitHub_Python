bb0c21ffa82044f2da7dcabf455a174d00d7b24c
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-18 08:50:13 -0400

    WORKS BLYYYYAAATTTTTT
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- go_back_to_blocks.py
+++ go_back_to_blocks.py
@@ -1,34 +1,13 @@
 from line_follower_class import *
-from threading import *
-
-lines_passed = False
 
 
 def go_back_to_blocks():
-    global lines_passed
-
-    def check_for_lines(num_lines):
-        global lines_passed
-        counter = 0
-        while counter < num_lines:
-            print(counter)
-            if center_sensor.reflected_light_intensity < 30:
-                if counter < num_lines - 1:
-                    counter = counter + 1
-                    sleep(0.3)
-                else:
-                    counter = counter + 1
-        lines_passed = True
 
     while right_side_sensor.reflected_light_intensity > 30:
         steer_pair.on(-100, -30)
     steer_pair.on_for_rotations(100, -20, 0.07)
     steer_pair.off()
-    t = Thread(target=check_for_lines, args=(2,))
-    t.start()
-    while not lines_passed:
-        losp_right_follower(speed=30)
-    lines_passed = False
+    follow_for_xlines(2, right_side_sensor, speed=40, kp=0.2, ttarget=40)
     steer_pair.off()
     steer_pair.on_for_rotations(0, -35, 0.3)
     while right_side_sensor.reflected_light_intensity > 30:
@@ -36,12 +15,8 @@ def go_back_to_blocks():
     steer_pair.on_for_rotations(-100, -20, 0.07)
     steer_pair.off()
     timed_follower(sensor=right_side_sensor, timemax=3.5, side_of_line=1, speed=60, kp=0.15, ttarget=50)
-    t = Thread(target=check_for_lines, args=(1,))
-    t.start()
-    while not lines_passed:
-        hisp_right_follower(side_of_line=1, speed=30)
+    follow_for_xlines(1, right_side_sensor, speed=40, kp=0.2, ttarget=40, side_of_line=1)
     steer_pair.off()
-    lines_passed = False
 
 
 go_back_to_blocks()
