0fb2001dbf37467ccc69083177d7de32670b2794
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-18 13:08:46 -0400

    WORKS BLYYYYAAATTTTTT
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- go_back_to_blocks.py
+++ go_back_to_blocks.py
@@ -3,11 +3,11 @@ from line_follower_class import *
 
 def go_back_to_blocks():
 
-    lower_motor.on_for_degrees(30, -90)
     while right_side_sensor.reflected_light_intensity > 30:
         steer_pair.on(-100, -30)
     steer_pair.on_for_rotations(100, -20, 0.07)
     steer_pair.off()
+    grabber_servo.on_for_degrees(30, 180)
     follow_for_xlines(2, right_side_sensor, speed=40, kp=0.2, ttarget=40)
     steer_pair.off()
     steer_pair.on_for_rotations(0, -35, 0.3)
@@ -15,20 +15,19 @@ def go_back_to_blocks():
         steer_pair.on(100, -20)
     steer_pair.on_for_rotations(-100, -20, 0.07)
     steer_pair.off()
-    timed_follower(sensor=right_side_sensor, timemax=3.5, side_of_line=1, speed=60, kp=0.15, ttarget=50)
-    follow_for_xlines(1, right_side_sensor, speed=40, kp=0.2, ttarget=40, side_of_line=1)
+    # timed_follower(sensor=right_side_sensor, timemax=3.5, side_of_line=1, speed=60, kp=0.15, ttarget=50)
+    follow_for_xlines(2, right_side_sensor, speed=40, kp=0.2, ttarget=40, side_of_line=1)
     steer_pair.off()
     steer_pair.on_for_rotations(0, -40, 0.5)
     steer_pair.on_for_rotations(-70, 40, 0.9)
-    while right_side_sensor.reflected_light_intensity > 30:
-        steer_pair.on(-70, 30)
+    while center_sensor.reflected_light_intensity > 30:
+        steer_pair.on(-70, 20)
     steer_pair.off()
-    steer_pair.on_for_rotations(-70, 30, 0.07)
+    steer_pair.on_for_rotations(0, 40, 0.3)
+    steer_pair.on_for_rotations(0, -40, 1)
+    grabber_servo.on_for_degrees(30, -90)
     lower_motor.off()
-    side_color_sensor.mode = 'RGB'
-    while not 100 > side_color_sensor.value(3) > 40:
-        hisp_right_follower(side_of_line=1, speed=40, kp=0.2)
-    steer_pair.off()
+    steer_pair.on_for_rotations(0, 40, 1.3)
 
 
 go_back_to_blocks()
