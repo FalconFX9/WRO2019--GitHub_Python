de00f62ede90a120d9580b50895950d75c4a7c7f
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-18 09:14:13 -0400

    WORKS BLYYYYAAATTTTTT
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- turn_test.py
+++ turn_test.py
@@ -1,8 +1,6 @@
 from line_follower_class import *
 from threading import *
 
-file_s = open('sensor_data.txt', 'w+')
-file_x = open('time_data.txt', 'w+')
 side_color_sensor.mode = 'RGB'
 block_is_black = False
 block_left = False
@@ -29,10 +27,7 @@ class BlackOrWhite:
 
     def look_at_blocks(self):
         global block_is_black, block_left, blocks_passed
-        start_time = time()
         while not block_is_black:
-            file_s.write(str(self.light_intensity) + '\n')
-            file_x.write(str(round((time() - start_time), 2)) + '\n')
             if side_color_sensor.value(3) > 100:
                 sleep(0.3)
             elif 100 > side_color_sensor.value(3) > 40:
@@ -52,11 +47,9 @@ class BlackOrWhite:
 def turn_and_pick_up():
     global block_left, block_is_black, stop_log, blocks_passed
     while not block_is_black:
-        hisp_right_follower(speed=40)
+        hisp_right_follower(speed=40, side_of_line=1)
     steer_pair.off()
     block_is_black = False
-    file_s.close()
-    file_x.close()
     steer_pair.on_for_rotations(0, -30, 0.1)
     follow_to_line(following_sensor=right_side_sensor, line_sensor=center_sensor, speed=30, kp=0.1)
     stop_log = True
@@ -67,14 +60,14 @@ def turn_and_pick_up():
     steer_pair.on_for_rotations(0, 30, 0.15)
     steer_pair.on_for_rotations(60, 40, 1.14)
     steer_pair.on_for_rotations(0, 20, 0.2)
-    lower_motor.on_for_degrees(10, 54)
+    lower_motor.on_for_degrees(10, 58)
     if not block_left:
         grabber_servo.on_for_degrees(10, -90)
         block_left = True
     else:
         grabber_servo.on_for_degrees(10, -180)
         block_left = False
-    timed_follower(sensor=center_sensor, timemax=0.65, speed=20, kp=0.2, side_of_line=1)
+    timed_follower(sensor=center_sensor, timemax=0.65, speed=20, kp=0.35, side_of_line=1)
     steer_pair.off()
     lower_motor.on_for_degrees(10, -52)
 
@@ -84,6 +77,7 @@ t = BlackOrWhite()
 t.start_t()
 t.start_t2()
 turn_and_pick_up()
-sleep(5)
-t.start_t2_5()
-turn_and_pick_up()
+lower_motor.off()
+# sleep(5)
+# t.start_t2_5()
+# turn_and_pick_up()
