55783bc328464897f3fd4251513e8059af6ae5fe
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-18 09:01:08 -0400

    WORKS BLYYYYAAATTTTTT
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- go_back_to_blocks.py
+++ go_back_to_blocks.py
@@ -18,9 +18,8 @@ def go_back_to_blocks():
     timed_follower(sensor=right_side_sensor, timemax=3.5, side_of_line=1, speed=60, kp=0.15, ttarget=50)
     follow_for_xlines(1, right_side_sensor, speed=40, kp=0.2, ttarget=40, side_of_line=1)
     steer_pair.off()
+    steer_pair.on_for_rotations(0, -40, 1)
     steer_pair.on_for_rotations(-70, 40, 0.9)
-    while right_side_sensor.reflected_light_intensity > 30:
-        steer_pair.on(-100, -30)
     steer_pair.off()
 
 
