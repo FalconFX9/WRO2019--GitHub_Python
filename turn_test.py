190442ecd633e1954110d0e0a438eb3ba864df7f
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-18 09:56:40 -0400

    WORKS BLYYYYAAATTTTTT
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- turn_test.py
+++ turn_test.py
@@ -5,25 +5,13 @@ side_color_sensor.mode = 'RGB'
 block_is_black = False
 block_left = False
 blocks_passed = 0
-stop_log = False
 
 
 class BlackOrWhite:
 
     def __init__(self):
         self.light_intensity = side_color_sensor.value(3)
-        self.t = Thread(target=self.count_blocks_passed, args=())
-        self.t2_5 = Thread(target=self.count_blocks_passed, args=())
         self.t2 = Thread(target=self.look_at_blocks, args=())
-
-    def count_blocks_passed(self):
-        global blocks_passed
-        blocks_passed = 0
-        while not (blocks_passed == 3 and stop_log):
-            if self.light_intensity > 40:
-                blocks_passed += 1
-                sleep(0.3)
-        print('Thread count_blocks_passed is finished')
 
     def look_at_blocks(self):
         global block_is_black, block_left, blocks_passed
@@ -34,25 +22,20 @@ class BlackOrWhite:
                 block_is_black = True
         print('Thread look_at_blocks is finished')
 
-    def start_t(self):
-        self.t.start()
-
     def start_t2(self):
         self.t2.start()
 
-    def start_t2_5(self):
-        self.t2_5.start()
-
 
 def turn_and_pick_up():
-    global block_left, block_is_black, stop_log, blocks_passed
+    global block_left, block_is_black, blocks_passed, t_time
+    start_time = time()
     while not block_is_black:
         hisp_right_follower(speed=40, kp=0.15)
     steer_pair.off()
+    t_time = time() - start_time
     block_is_black = False
     steer_pair.on_for_rotations(0, -30, 0.1)
     follow_to_line(following_sensor=right_side_sensor, line_sensor=center_sensor, speed=30, kp=0.1)
-    stop_log = True
     if blocks_passed > 2:
         steer_pair.on_for_rotations(0, 30, 0.3)
     steer_pair.off()
@@ -75,7 +58,6 @@ def turn_and_pick_up():
 lower_motor.off()
 lower_motor.on_for_degrees(10, -3)
 t = BlackOrWhite()
-t.start_t()
 t.start_t2()
 turn_and_pick_up()
 steer_pair.on_for_rotations(0, -40, 0.3)
@@ -83,6 +65,13 @@ steer_pair.on_for_rotations(-70, 40, 0.7)
 while right_side_sensor.reflected_light_intensity > 30:
     steer_pair.on(-70, 30)
 steer_pair.off()
+if t_time > 1:
+    follow_for_xlines(3, sensor=right_side_sensor, side_of_line=1, speed=50, ttarget=45)
+    steer_pair.off()
+else:
+    follow_for_xlines(2, sensor=right_side_sensor, side_of_line=1, speed=50, ttarget=45)
+    steer_pair.off()
+'''
 while not 100 > side_color_sensor.value(3) > 40:
     hisp_right_follower(speed=30, kp=0.1)
 steer_pair.off(brake=False)
@@ -101,4 +90,4 @@ timed_follower(sensor=center_sensor, timemax=0.65, speed=20, kp=0.35, side_of_li
 steer_pair.off()
 lower_motor.on_for_degrees(10, -52)
 lower_motor.off()
-
+'''
