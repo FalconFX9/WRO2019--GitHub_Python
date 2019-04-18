e5a66bf92e97a09df8b0f46827124b003d14550a
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-18 11:05:10 -0400

    WORKS BLYYYYAAATTTTTT
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- WRO2019.py
+++ WRO2019.py
@@ -2,8 +2,6 @@
 from regrouped_functions import wro2019
 from sensor_and_motor_startup import *
 
-while not Button.buttons_pressed:
-    pass
 wro2019()
 steer_pair.off(brake=False)
 lower_motor.off(brake=False)
