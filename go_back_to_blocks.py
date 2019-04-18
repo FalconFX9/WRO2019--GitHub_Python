3915935a7741377b32a0201d863996caf1be8c56
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-04-18 13:13:01 -0400

    WORKS BLYYYYAAATTTTTT
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- go_back_to_blocks.py
+++ go_back_to_blocks.py
@@ -3,6 +3,7 @@ from line_follower_class import *
 
 def go_back_to_blocks():
 
+    lower_motor.on_for_degrees(30, -85)
     while right_side_sensor.reflected_light_intensity > 30:
         steer_pair.on(-100, -30)
     steer_pair.on_for_rotations(100, -20, 0.07)
@@ -18,13 +19,13 @@ def go_back_to_blocks():
     # timed_follower(sensor=right_side_sensor, timemax=3.5, side_of_line=1, speed=60, kp=0.15, ttarget=50)
     follow_for_xlines(2, right_side_sensor, speed=40, kp=0.2, ttarget=40, side_of_line=1)
     steer_pair.off()
-    steer_pair.on_for_rotations(0, -40, 0.5)
+    steer_pair.on_for_rotations(0, -40, 0.75)
     steer_pair.on_for_rotations(-70, 40, 0.9)
     while center_sensor.reflected_light_intensity > 30:
         steer_pair.on(-70, 20)
     steer_pair.off()
     steer_pair.on_for_rotations(0, 40, 0.3)
-    steer_pair.on_for_rotations(0, -40, 1)
+    steer_pair.on_for_rotations(0, -40, 0.7)
     grabber_servo.on_for_degrees(30, -90)
     lower_motor.off()
     steer_pair.on_for_rotations(0, 40, 1.3)
