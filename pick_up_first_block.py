ab5ecc5ad0ff44b59aefe33ad6fa2f27f2fa38d3
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-05-01 16:00:34 -0400

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- pick_up_first_block.py
+++ pick_up_first_block.py
@@ -8,7 +8,6 @@ def pick_up_block():
     while not 100 > side_color_sensor.value(3) > 40:
         hisp_right_follower(speed=40, kp=0.15)
     steer_pair.off()
-    steer_pair.on_for_rotations(0, -30, 0.1)
     timed_follower(sensor=right_side_sensor, speed=30, kp=0.15, timemax=0.33)
     lower_motor.on_for_degrees(10, -10)
     steer_pair.on_for_rotations(60, 40, 1.2)
