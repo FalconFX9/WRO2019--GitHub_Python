9d98ed0f5fe8e6fd68c1e7d5fad7ba6a64f7168c
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-05-01 16:23:26 -0400

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- pick_up_first_block.py
+++ pick_up_first_block.py
@@ -1,15 +1,28 @@
 from line_follower_class import *
 
 side_color_sensor.mode = 'RGB'
+block_is_black = False
+
+
+def look_at_blocks():
+    global block_is_black
+    while not block_is_black:
+        if side_color_sensor.value(3) > 100:
+            sleep(0.3)
+        elif 100 > side_color_sensor.value(3) > 40:
+            block_is_black = True
+    print('Thread look_at_blocks is finished')
 
 
 def pick_up_block():
+    global block_is_black
     lower_motor.off()
-    while not 100 > side_color_sensor.value(3) > 40:
+    while not block_is_black:
         hisp_right_follower(speed=40, kp=0.15)
         print(side_color_sensor.value(3))
     steer_pair.off()
-    timed_follower(sensor=right_side_sensor, speed=30, kp=0.15, timemax=0.33)
+    follow_to_line(right_side_sensor, center_sensor, 30, kp=0.2)
+    steer_pair.on_for_rotations(0, 30, 0.3)
     lower_motor.on_for_degrees(10, -10)
     steer_pair.on_for_rotations(60, 40, 1.2)
     steer_pair.on_for_rotations(0, 20, 0.2)
@@ -20,12 +33,4 @@ def pick_up_block():
     lower_motor.on_for_degrees(10, -52)
 
 
-lower_motor.off()
-while True:
-    try:
-        hisp_right_follower(speed=40)
-        file_s.write(str(side_color_sensor.value(3)) + '\n')
-        file_x.write(str(round((time()), 1)) + '\n')
-    except KeyboardInterrupt:
-        file_s.close()
-        file_x.close()
+pick_up_block()
