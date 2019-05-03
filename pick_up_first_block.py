0ff31cb62e22796cab5db87f02f60e27f432125e
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-05-03 15:37:12 -0400

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- pick_up_first_block.py
+++ pick_up_first_block.py
@@ -3,6 +3,7 @@ from threading import Thread
 
 side_color_sensor.mode = 'RGB'
 block_is_black = False
+t_time = 0
 
 
 def look_at_blocks():
@@ -16,11 +17,13 @@ def look_at_blocks():
 
 
 def pick_up_block():
-    global block_is_black
+    global block_is_black, t_time
     lower_motor.off()
+    start_time = time()
     while not block_is_black:
         hisp_right_follower(speed=40, kp=0.15)
     steer_pair.off()
+    t_time = time() - start_time
     steer_pair.on_for_rotations(0, -30, 0.1)
     follow_to_line(right_side_sensor, center_sensor, 30, kp=0.2)
     steer_pair.off()
@@ -34,5 +37,45 @@ def pick_up_block():
     lower_motor.on_for_degrees(10, -52)
 
 
+def go_to_put_down():
+    global t_time
+    steer_pair.on_for_rotations(0, -40, 0.3)
+    steer_pair.on_for_rotations(-70, 40, 0.7)
+    while right_side_sensor.reflected_light_intensity > 30:
+        steer_pair.on(-70, 30)
+    steer_pair.off()
+    if t_time < 1:
+        follow_for_xlines(3, sensor=right_side_sensor, speed=50, ttarget=45, kp=0.2)
+        steer_pair.off()
+    else:
+        follow_for_xlines(2, sensor=right_side_sensor, speed=50, ttarget=45, kp=0.2)
+        steer_pair.off()
+    steer_pair.on_for_rotations(0, -40, 0.2)
+    steer_pair.on_for_rotations(-70, 40, 0.7)
+    while right_side_sensor.reflected_light_intensity > 30:
+        steer_pair.on(-70, 30)
+    steer_pair.off()
+    steer_pair.on_for_rotations(-70, 40, 0.1)
+    follow_to_line(following_sensor=right_side_sensor, line_sensor=center_sensor, speed=40, kp=0.65)
+    steer_pair.off()
+    steer_pair.on_for_rotations(0, -40, 0.65)
+    steer_pair.on_for_rotations(-70, 40, 0.7)
+    while center_sensor.reflected_light_intensity > 30:
+        steer_pair.on(-70, 30)
+    steer_pair.off()
+    steer_pair.on_for_rotations(70, 40, 0.05)
+    follow_to_line(following_sensor=center_sensor, line_sensor=right_side_sensor, side_of_line=1, speed=30, kp=0.4)
+    steer_pair.off()
+    steer_pair.on_for_rotations(0, -40, 0.65)
+    steer_pair.on_for_rotations(70, 40, 0.7)
+    while center_sensor.reflected_light_intensity > 30:
+        steer_pair.on(70, 20)
+    steer_pair.off()
+    steer_pair.on_for_rotations(-70, 30, 0.05)
+    timed_follower(center_sensor, 1, speed=30, kp=0.4)
+    steer_pair.off()
+
+
 Thread(target=look_at_blocks).start()
 pick_up_block()
+go_to_put_down()
