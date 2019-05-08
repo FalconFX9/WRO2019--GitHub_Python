ec89522032ac6edf7ebe7ea76655572fee4aec5e
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-05-08 16:22:33 -0400

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- pick_up_first_block.py
+++ pick_up_first_block.py
@@ -81,8 +81,10 @@ def go_to_put_down():
 
 def put_down_blocks(block_pos: int):
     grabber_servo.on_for_degrees(30, block_pos)
-    sleep(1)
+    sleep(0.7)
+    steer_pair.on_for_rotations(0, 30, 0.1)
     lower_motor.on_for_degrees(10, 40)
+    sleep(0.5)
     grabber_servo.on_for_degrees(20, 180)
 
 
