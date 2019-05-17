1eb35341698e01819f1b0e800fce21ff29fd442a
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-05-17 17:16:13 -0400

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- get_and_put_down_red_block.py
+++ get_and_put_down_red_block.py
@@ -2,4 +2,18 @@ from pick_up_first_block import *
 
 
 def go_back_to_pickup():
-    pass
+    while right_side_sensor.reflected_light_intensity > 30:
+        steer_pair.on(-100, -30)
+    steer_pair.on_for_rotations(100, -20, 0.07)
+    steer_pair.off()
+    grabber_servo.on_for_degrees(30, 180)
+    follow_for_xlines(1, right_side_sensor, speed=40, kp=0.2, ttarget=40)
+    steer_pair.off()
+    steer_pair.on_for_rotations(0, -35, 0.3)
+    while right_side_sensor.reflected_light_intensity > 30:
+        steer_pair.on(-100, -20)
+    steer_pair.on_for_rotations(-100, 20, 0.07)
+    steer_pair.off()
+
+
+go_back_to_pickup()
