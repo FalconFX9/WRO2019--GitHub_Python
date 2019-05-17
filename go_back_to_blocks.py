3ba864ceea2096c0d845c562a45688fd235b42d2
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-05-17 19:37:14 -0400

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- go_back_to_blocks.py
+++ go_back_to_blocks.py
@@ -7,7 +7,6 @@ def go_back_to_blocks():
         steer_pair.on(-100, -30)
     steer_pair.on_for_rotations(100, -20, 0.07)
     steer_pair.off()
-    grabber_servo.on_for_degrees(30, 180)
     follow_for_xlines(2, right_side_sensor, speed=40, kp=0.2, ttarget=40)
     steer_pair.off()
     steer_pair.on_for_rotations(0, -35, 0.3)
