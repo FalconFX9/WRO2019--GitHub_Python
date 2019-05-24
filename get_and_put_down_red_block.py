74417f6f17190e1da8074693b538b67b3f2a430d
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-05-24 16:34:39 -0400

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- get_and_put_down_red_block.py
+++ get_and_put_down_red_block.py
@@ -9,7 +9,6 @@ def go_back_to_pickup():
         steer_pair.on(-100, -30)
     steer_pair.on_for_rotations(100, -20, 0.07)
     steer_pair.off()
-    grabber_servo.on_for_degrees(30, 180)
     follow_for_xlines(1, right_side_sensor, speed=40, kp=0.2, ttarget=40)
     steer_pair.off()
     steer_pair.on_for_rotations(0, -35, 0.3)
@@ -25,6 +24,17 @@ def go_back_to_pickup():
     elif blocks[1] == 'black':
         follow_for_xlines(3, left_side_sensor, 1)
     steer_pair.off()
+    lower_motor.on_for_degrees(10, -10)
+    steer_pair.on_for_rotations(-60, 40, 1.2)
+    while center_sensor.reflected_light_intensity > 30:
+        steer_pair.on(-60, 15)
+    steer_pair.off()
+    steer_pair.on_for_rotations(0, 20, 0.2)
+    lower_motor.on_for_degrees(20, 58)
+    sleep(0.6)
+    timed_follower(sensor=center_sensor, timemax=0.45, speed=40, kp=0.25, ttarget=30)
+    steer_pair.off()
+    lower_motor.on_for_degrees(10, -52)
 
 
 go_back_to_pickup()
