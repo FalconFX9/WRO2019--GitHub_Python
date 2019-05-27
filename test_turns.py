367c158aa216f0c130d9d6b0fcdbb1a47d87d2e7
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2019-05-27 19:41:20 -0400

    Testing
    Signed-off-by: Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>

--- test_turns.py
+++ test_turns.py
@@ -1,4 +1,8 @@
 from ev3dev2.auto import *
+from line_follower_class import *
 
 steer_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
-steer_pair.on_for_rotations(100, 40, 2)
+steer_pair.on_for_rotations(100, 40, 1)
+while not center_sensor.reflected_light_intensity < 30:
+    steer_pair.on(100, 40)
+steer_pair.off()
